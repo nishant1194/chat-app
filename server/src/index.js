d571cf35b74f08bd072d37cd421f6359c23f27d9
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2025-01-03 19:51:10 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "*", // Allow your frontend's URL
+    origin: "http://localhost:5173", // Allow your frontend's URL
     credentials: true,              // Allow cookies or authorization headers
     methods: "GET, POST, PUT, DELETE", // Specify allowed HTTP methods
     allowedHeaders: "Content-Type, Authorization", // Allowed headers
