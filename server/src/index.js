d56b4c2bfd9203ad64e41f076eaef47cfecf4c73
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2025-01-03 19:47:00 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
   app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
