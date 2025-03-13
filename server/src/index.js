c2dd1c82eed2ebc2b4ce06b7821892ad7a187449
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2025-03-13 19:27:27 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -17,10 +17,15 @@ const PORT = process.env.PORT || 5001;
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
   app.use(cookieParser());
   
-app.use(cors({
-    origin: '*',
-    credentials: true
-  }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",  
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 
 
 app.use("/api/auth", authRoutes);
